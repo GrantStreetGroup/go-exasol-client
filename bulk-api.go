GrantStreetGroup/go-exasol-client

commit 719cee541c48b4eab43dc1ca846709f622acfe63

Fix data races when using multiple Conns in parallel.
Since there was one global log instance, parallel access lead to internal
data races in go-logging. AFAIK there are no parallelism guarantees
with go-logging.
Thus replaced global log instance with field in Conn/Proxy.

bulk-api.go
@@ -64,7 +64,7 @@ func (c *Conn) BulkInsert(schema, table string, data *bytes.Buffer) (err error) 
 
 func (c *Conn) BulkExecute(sql string, data *bytes.Buffer) error {
 	if data == nil {
-		log.Fatal("You must pass in a bytes.Buffer pointer to BulkExecute")
+		c.log.Fatal("You must pass in a bytes.Buffer pointer to BulkExecute")
 	}
 	dataChan := make(chan []byte, 1)
 	dataChan <- data.Bytes()
@@ -79,7 +79,7 @@ func (c *Conn) BulkSelect(schema, table string, data *bytes.Buffer) (err error) 
 
 func (c *Conn) BulkQuery(sql string, data *bytes.Buffer) error {
 	if data == nil {
-		log.Fatal("You must pass in a bytes.Buffer pointer to BulkQuery")
+		c.log.Fatal("You must pass in a bytes.Buffer pointer to BulkQuery")
 	}
 	dataChan := make(chan []byte, 1)
 	go func() {
@@ -98,7 +98,7 @@ func (c *Conn) StreamInsert(schema, table string, data <-chan []byte) (err error
 
 func (c *Conn) StreamExecute(origSQL string, data <-chan []byte) error {
 	if data == nil {
-		log.Fatal("You must pass in a []byte chan to StreamExecute")
+		c.log.Fatal("You must pass in a []byte chan to StreamExecute")
 	}
 
 	sentData := false
@@ -130,7 +130,7 @@ func (c *Conn) StreamExecute(origSQL string, data <-chan []byte) error {
 			Command: "execute",
 			SQLtext: sql,
 		}
-		log.Info("EXA: Execute Import: ", sql)
+		c.log.Info("EXA: Execute Import: ", sql)
 		response, err := c.asyncSend(req)
 		if err != nil {
 			c.error("Unable to import bulk import data:", sql, err)
@@ -169,7 +169,7 @@ func (c *Conn) StreamSelect(schema, table string, data chan<- []byte) (int64, er
 
 func (c *Conn) StreamQuery(origSQL string, data chan<- []byte) (int64, error) {
 	if data == nil {
-		log.Fatal("You must pass in a []byte chan to StreamQuery")
+		c.log.Fatal("You must pass in a []byte chan to StreamQuery")
 	}
 
 	var bytesRead int64
@@ -195,7 +195,7 @@ func (c *Conn) StreamQuery(origSQL string, data chan<- []byte) (int64, error) {
 			Command: "execute",
 			SQLtext: sql,
 		}
-		log.Info("EXA: Execute Execute: ", sql)
+		c.log.Info("EXA: Execute Execute: ", sql)
 		response, err := c.asyncSend(req)
 		if err != nil {
 			c.error("Unable to bulk export data:", sql, err)

proxy.go
@@ -21,16 +21,21 @@ import (
 	"fmt"
 	"net"
 	"strconv"
+
+	"github.com/op/go-logging"
 )
 
 type Proxy struct {
 	conn net.Conn
 	Host string
 	Port uint32
+	log  *logging.Logger
 }
 
 func NewProxy(host string, port uint16) (*Proxy, error) {
-	p := &Proxy{}
+	p := &Proxy{
+		log: logging.MustGetLogger("exasol"),
+	}
 
 	var err error
 	uri := fmt.Sprintf("%s:%d", host, port)
@@ -58,7 +63,7 @@ func NewProxy(host string, port uint16) (*Proxy, error) {
 
 	p.Port = binary.LittleEndian.Uint32(resp[4:])
 	p.Host = string(bytes.Trim(resp[8:], "\x00")) // Remove nulls
-	log.Debugf("EXA: Proxy is %s:%d", p.Host, p.Port)
+	p.log.Debugf("EXA: Proxy is %s:%d", p.Host, p.Port)
 
 	return p, nil
 }
@@ -71,7 +76,7 @@ func (p *Proxy) Read(data chan<- []byte) (int64, error) {
 			return 0, fmt.Errorf("Unable to read from proxy(1): %s", err)
 		}
 		// blank line means end of headers
-		log.Debug("Got header:", string(line))
+		p.log.Debug("Got header:", string(line))
 		if len(line) == 0 {
 			break
 		}
@@ -185,7 +190,7 @@ func (p *Proxy) sendHeaders(headers []string) error {
 	headers = append(headers, "")
 	for _, header := range headers {
 		header += "\r\n"
-		log.Debug("Sent Header: ", header)
+		p.log.Debug("Sent Header: ", header)
 		_, err := p.conn.Write([]byte(header))
 		if err != nil {
 			return fmt.Errorf("Unable to send header <%s>to proxy: %s", header, err)

client.go
@@ -38,8 +38,6 @@ import (
 	"github.com/op/go-logging"
 )
 
-var log = logging.MustGetLogger("exasol")
-
 /*--- Public Interface ---*/
 
 type ConnConf struct {
@@ -59,6 +57,7 @@ type Conn struct {
 	SessionID uint64
 	Stats     map[string]int
 
+	log           *logging.Logger
 	ws            *websocket.Conn
 	prepStmtCache map[string]*prepStmt
 }
@@ -72,13 +71,14 @@ type DataType struct {
 }
 
 func Connect(conf ConnConf) *Conn {
-	initLogging(conf.LogLevel)
 
 	c := &Conn{
 		Conf:          conf,
 		Stats:         map[string]int{},
+		log:           logging.MustGetLogger("exasol"),
 		prepStmtCache: map[string]*prepStmt{},
 	}
+	c.initLogging(conf.LogLevel)
 
 	// Both of these are fatal if they encounter an error
 	c.wsConnect()
@@ -87,28 +87,28 @@ func Connect(conf ConnConf) *Conn {
 }
 
 func (c *Conn) Disconnect() {
-	log.Notice("EXA: Disconnecting SessionID:", c.SessionID)
+	c.log.Notice("EXA: Disconnecting SessionID:", c.SessionID)
 
 	for _, ps := range c.prepStmtCache {
 		c.closePrepStmt(ps.sth)
 	}
 	_, err := c.send(&disconnectJSON{Command: "disconnect"})
 	if err != nil {
-		log.Warning("Unable to disconnect from EXASOL: ", err)
+		c.log.Warning("Unable to disconnect from EXASOL: ", err)
 	}
 	c.ws.Close()
 	c.ws = nil
 }
 
 func (c *Conn) GetSessionAttr() (map[string]interface{}, error) {
-	log.Info("EXA: Getting Attr")
+	c.log.Info("EXA: Getting Attr")
 	req := &sendAttrJSON{Command: "getAttributes"}
 	res, err := c.send(req)
 	return res, err
 }
 
 func (c *Conn) EnableAutoCommit() {
-	log.Info("EXA: Enabling AutoCommit")
+	c.log.Info("EXA: Enabling AutoCommit")
 	c.send(&sendAttrJSON{
 		Command:    "setAttributes",
 		Attributes: attrJSON{AutoCommit: true},
@@ -116,7 +116,7 @@ func (c *Conn) EnableAutoCommit() {
 }
 
 func (c *Conn) DisableAutoCommit() {
-	log.Info("EXA: Disabling AutoCommit")
+	c.log.Info("EXA: Disabling AutoCommit")
 	// We have to roll our own map because attrJSON
 	// needs to have AutoCommit set to omitempty which
 	// causes autocommit=false not to be sent :-(
@@ -129,13 +129,13 @@ func (c *Conn) DisableAutoCommit() {
 }
 
 func (c *Conn) Rollback() error {
-	log.Info("EXA: Rolling back transaction")
+	c.log.Info("EXA: Rolling back transaction")
 	_, err := c.Execute("ROLLBACK")
 	return err
 }
 
 func (c *Conn) Commit() error {
-	log.Info("EXA: Committing transaction")
+	c.log.Info("EXA: Committing transaction")
 	_, err := c.Execute("COMMIT")
 	return err
 }
@@ -164,7 +164,7 @@ func (c *Conn) Execute(sql string, args ...interface{}) (map[string]interface{},
 	// Just a simple execute (no prepare) if there are no binds
 	if binds == nil || len(binds) == 0 ||
 		binds[0] == nil || len(binds[0]) == 0 {
-		log.Info("EXA: Execute Query: ", sql)
+		c.log.Info("EXA: Execute Query: ", sql)
 		req := &executeStmtJSON{
 			Command:    "execute",
 			Attributes: attrJSON{CurrentSchema: schema},
@@ -191,7 +191,7 @@ func (c *Conn) Execute(sql string, args ...interface{}) (map[string]interface{},
 	numCols := len(binds)
 	numRows := len(binds[0])
 
-	log.Infof("EXA: Executing %d x %d stmt", numCols, numRows)
+	c.log.Infof("EXA: Executing %d x %d stmt", numCols, numRows)
 	execReq := &execPrepStmtJSON{
 		Command:         "executePreparedStatement",
 		StatementHandle: int(ps.sth),
@@ -206,13 +206,13 @@ func (c *Conn) Execute(sql string, args ...interface{}) (map[string]interface{},
 	if err != nil &&
 		regexp.MustCompile("Statement handle not found").MatchString(err.Error()) {
 		// Not sure what causes this but I've seen it happen. So just try again.
-		log.Warning("Statement handle not found:", ps.sth)
+		c.log.Warning("Statement handle not found:", ps.sth)
 		delete(c.prepStmtCache, sql)
 		ps, err := c.getPrepStmt(schema, sql)
 		if err != nil {
 			return nil, err
 		}
-		log.Warning("Retrying with:", ps.sth)
+		c.log.Warning("Retrying with:", ps.sth)
 		execReq.StatementHandle = int(ps.sth)
 		res, err = c.send(execReq)
 	}
@@ -235,7 +235,7 @@ func (c *Conn) FetchChan(sql string, args ...interface{}) (<-chan []interface{},
 		return nil, err
 	}
 	if response["numResults"].(float64) != 1 {
-		log.Fatalf("Unexpected numResults: %s", response["numResults"].(float64))
+		c.log.Fatalf("Unexpected numResults: %s", response["numResults"].(float64))
 	}
 	results := response["results"].([]interface{})[0].(map[string]interface{})
 	// TODO die gracefully if there is no resultSet. This happens, for instance,
@@ -257,7 +257,7 @@ func (c *Conn) FetchChan(sql string, args ...interface{}) (<-chan []interface{},
 				}
 				chunk, err := c.send(fetchReq)
 				if err != nil {
-					log.Panic(err)
+					c.log.Panic(err)
 				}
 				i += chunk["numRows"].(float64)
 				transposeToChan(ch, chunk["data"].([]interface{}))
@@ -269,7 +269,7 @@ func (c *Conn) FetchChan(sql string, args ...interface{}) (<-chan []interface{},
 			}
 			_, err = c.send(closeRSReq)
 			if err != nil {
-				log.Warning("Unable to close result set:", err)
+				c.log.Warning("Unable to close result set:", err)
 			}
 		} else {
 			transposeToChan(ch, rs["data"].([]interface{}))
@@ -351,13 +351,13 @@ type closeResultSetJSON struct {
 	ResultSetHandles []float64 `json:"resultSetHandles"`
 }
 
-func initLogging(logLevelStr string) {
+func (c *Conn) initLogging(logLevelStr string) {
 	if logLevelStr == "" {
 		logLevelStr = "error"
 	}
 	logLevel, err := logging.LogLevel(logLevelStr)
 	if err != nil {
-		log.Fatal("Unrecognized log level", err)
+		c.log.Fatal("Unrecognized log level", err)
 	}
 	logFormat := logging.MustStringFormatter(
 		"%{color}%{time:15:04:05.000} %{shortfunc}: " +
@@ -367,18 +367,18 @@ func initLogging(logLevelStr string) {
 	formattedBackend := logging.NewBackendFormatter(backend, logFormat)
 	leveledBackend := logging.AddModuleLevel(formattedBackend)
 	leveledBackend.SetLevel(logLevel, "exasol")
-	log.SetBackend(leveledBackend)
+	c.log.SetBackend(leveledBackend)
 }
 
 func (c *Conn) login() {
-	log.Notice("EXA: Logging in")
+	c.log.Notice("EXA: Logging in")
 	loginReq := &loginJSON{
 		Command:         "login",
 		ProtocolVersion: 1,
 	}
 	res, err := c.send(loginReq) // TODO change req to pointer
 	if err != nil {
-		log.Fatal("Unable to login to EXASOL: ", err)
+		c.log.Fatal("Unable to login to EXASOL: ", err)
 	}
 
 	pubKeyMod, _ := hex.DecodeString(res["publicKeyModulus"].(string))
@@ -394,13 +394,13 @@ func (c *Conn) login() {
 	password := []byte(c.Conf.Password)
 	encPass, err := rsa.EncryptPKCS1v15(rand.Reader, &pubKey, password)
 	if err != nil {
-		log.Fatal("Pass Enc error:", err, ": ", pubKeyExp)
+		c.log.Fatal("Pass Enc error:", err, ": ", pubKeyExp)
 	}
 	b64Pass := base64.StdEncoding.EncodeToString(encPass)
 
 	osUser, _ := user.Current()
 
-	log.Notice("EXA: Authenticating")
+	c.log.Notice("EXA: Authenticating")
 	authReq := &authJSON{
 		Username:         c.Conf.Username,
 		Password:         b64Pass,
@@ -413,9 +413,9 @@ func (c *Conn) login() {
 	}
 	resp, err := c.send(authReq)
 	if err != nil {
-		log.Fatal("Unable authenticate with EXASOL: ", err)
+		c.log.Fatal("Unable authenticate with EXASOL: ", err)
 	}
 	c.SessionID = uint64(resp["sessionId"].(float64))
-	log.Notice("EXA: Connected SessionID:", c.SessionID)
+	c.log.Notice("EXA: Connected SessionID:", c.SessionID)
 	c.ws.EnableWriteCompression(false)
 }

prep-stmt.go
@@ -50,7 +50,7 @@ func (c *Conn) getPrepStmt(schema, sql string) (*prepStmt, error) {
 	//      doesn't match the passed in data (i.e. placeholder/binds mismatch)
 	//      otherwise results in lowerlevel websocket closure
 
-	log.Info("EXA: Preparing stmt for:", sql)
+	c.log.Info("EXA: Preparing stmt for:", sql)
 	psc := c.prepStmtCache
 	ps := psc[sql]
 	if ps == nil {
@@ -100,12 +100,12 @@ func (c *Conn) createPrepStmt(schema string, sql string) (*prepStmt, error) {
 	sth := resp["statementHandle"].(float64)
 	paramData := resp["parameterData"].(map[string]interface{})
 	columnDefs := paramData["columns"].([]interface{})
-	log.Info("EXA: Got stmt handle ", sth)
+	c.log.Info("EXA: Got stmt handle ", sth)
 	return &prepStmt{sth, time.Now(), columnDefs}, nil
 }
 
 func (c *Conn) closePrepStmt(sth float64) {
-	log.Info("EXA: Closing stmt handle ", sth)
+	c.log.Info("EXA: Closing stmt handle ", sth)
 	closeReq := &closePrepStmtJSON{
 		Command:         "closePreparedStatement",
 		StatementHandle: int(sth),

utils.go
@@ -22,6 +22,7 @@ import (
 	"sync"
 
 	"github.com/davecgh/go-spew/spew"
+	"github.com/op/go-logging"
 )
 
 var keywordLock sync.RWMutex
@@ -75,7 +76,7 @@ func Transpose(matrix [][]interface{}) [][]interface{} {
 
 func (c *Conn) error(args ...interface{}) {
 	if c.Conf.SuppressError == false {
-		log.Error(args...)
+		c.log.Error(args...)
 	}
 }
 
@@ -91,14 +92,14 @@ func transposeToChan(ch chan<- []interface{}, matrix []interface{}) {
 }
 
 // For debugging
-func warnJson(msg interface{}) {
+func warnJson(l *logging.Logger, msg interface{}) {
 	json, _ := json.Marshal(msg)
-	log.Errorf("WARNING: %s", json)
+	l.Errorf("WARNING: %s", json)
 }
 
-func dieJson(msg interface{}) {
+func dieJson(l *logging.Logger, msg interface{}) {
 	json, _ := json.Marshal(msg)
-	log.Panicf("DIEING: %s", json)
+	l.Panicf("DIEING: %s", json)
 }
 
 func dump(i interface{}) {

websocket.go
@@ -35,13 +35,13 @@ func (c *Conn) wsConnect() {
 		Scheme: "ws",
 		Host:   uri,
 	}
-	log.Debugf("EXA: connecting to %s", u.String())
+	c.log.Debugf("EXA: connecting to %s", u.String())
 	// According to documentation:
 	// > It is safe to call Dialer's methods concurrently.
 	ws, resp, err := defaultDialer.Dial(u.String(), nil)
 	if err != nil {
-		log.Debugf("resp:%s", resp)
-		log.Fatal("dial:", err)
+		c.log.Debugf("resp:%s", resp)
+		c.log.Fatal("dial:", err)
 	}
 	c.ws = ws
 }
@@ -67,7 +67,7 @@ func (c *Conn) asyncSend(request interface{}) (func() (map[string]interface{}, e
 		err = c.ws.ReadJSON(&response)
 
 		if err != nil {
-			log.Panicf("Error recving: %s", err)
+			c.log.Panicf("Error recving: %s", err)
 		} else if response["status"] != "ok" {
 			exception := response["exception"].(map[string]interface{})
 			err = errors.New(exception["text"].(string))

commit 2c232a92c513b5a5988ad078aa82fe568e6080f9

Logging in Proxy now behaves as for Client
Previous change in 719cee541c48b4eab43dc1ca846709f622acfe63 broke the
logging behavior of Proxy.

bulk-api.go
@@ -115,7 +115,7 @@ func (c *Conn) StreamExecute(origSQL string, data <-chan []byte) error {
 			c.error("Retrying...")
 		}
 
-		proxy, err := NewProxy(c.Conf.Host, c.Conf.Port)
+		proxy, err := NewProxy(c.Conf.Host, c.Conf.Port, c.log)
 		if err != nil {
 			c.error(err)
 			lastErr = err
@@ -180,7 +180,7 @@ func (c *Conn) StreamQuery(origSQL string, data chan<- []byte) (int64, error) {
 			c.error("Retrying...")
 		}
 
-		proxy, err := NewProxy(c.Conf.Host, c.Conf.Port)
+		proxy, err := NewProxy(c.Conf.Host, c.Conf.Port, c.log)
 		if err != nil {
 			c.error(err)
 			lastErr = err

proxy.go
@@ -32,9 +32,11 @@ type Proxy struct {
 	log  *logging.Logger
 }
 
-func NewProxy(host string, port uint16) (*Proxy, error) {
+// NewProxy must never be used concurrently with the creating Conn.
+// Violating this rule will introduce data races into your program.
+func NewProxy(host string, port uint16, log *logging.Logger) (*Proxy, error) {
 	p := &Proxy{
-		log: logging.MustGetLogger("exasol"),
+		log: log,
 	}
 
 	var err error